87b9690f7b0f60e743456d0739cb4b2c57b6c69b
Guilherme Gules Moreira <guilherme.moreira@stone.com.br>
2025-10-01 16:26:31 -0300

    feat: add a more complex tree with jsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,38 +2,10 @@ import { useEffect, useRef, useState } from "@hooks";
 import { createElement, render } from "@vdom";
 
 function Button(props: { onClick: () => void; label: string }) {
-  return createElement(
-    "button",
-    { onclick: props.onClick, className: "btn-primary" },
-    props.label
-  );
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  return createElement(
-    "div",
-    null,
-    createElement("p", null, `Count: ${count}`),
-    createElement(Button, {
-      onClick: () => setCount(count + 1),
-      label: "Increment",
-    })
-  );
+  return <button onclick={props.onClick}>{props.label}</button>;
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  const buttonRef = useRef({});
-
-  useEffect(() => {
-    console.log("Mounted or count changed:", count);
-    return () => {
-      console.log("Cleanup effect");
-    };
-  }, [count]);
-
   return (
     <div id="app-container">
       <h1>Mini VDOM</h1>
@@ -43,18 +15,7 @@ function App() {
         <li>Test with complex trees</li>
       </ul>
       <Button onClick={() => alert("clicked")} label="Click"></Button>
-      <button
-        onClick={() => {
-          setCount((c) => c + 1);
-        }}
-        ref={(element: HTMLButtonElement) => {
-          buttonRef.current = element;
-          console.log("Button element", element);
-        }}
-      >
-        count {count}
-      </button>
-      <Counter />
+      <button>count TODO</button>
       <footer>Some footer text here</footer>
     </div>
   );
